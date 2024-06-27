90d135679fcffdb96ed64d9f045123d8e8d40fd9
Julian Laballe <julian.laballe@unlikely.technology>
2024-06-27 15:36:00 +0200

    feat: make first test fail

--- src/main/java/hetic/RomanNumber.java
+++ src/main/java/hetic/RomanNumber.java
@@ -4,4 +4,8 @@ public class RomanNumber {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static String toto(Integer number) {
+        return "12";
+    }
 }
--- src/test/java/TestRomanNumber.java
+++ src/test/java/TestRomanNumber.java
@@ -1,5 +1,11 @@
+import hetic.RomanNumber;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestRomanNumber {
+    @Test
     void should_return_1_for_1() {
-        a
+        assertEquals("1", RomanNumber.toto(1));
     }
 }
